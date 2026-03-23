3c25a1bd92d2875cae1a2bdd6fb14e25c2b53046
mohammadnoman365 <nomanmohammad365@gmail.com>
2026-03-23 05:09:01 +0500

    Update SceneLoader.cs

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -14,16 +14,6 @@ public class SceneLoader : MonoBehaviour
         StartCoroutine(LoadSceneAsyncronously(sceneName));
     }
 
-    //IEnumerator LoadSceneAsyncronously(string sceneName)
-    //{
-    //    AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
-    //    while (!operation.isDone)
-    //    {
-    //        loadingScreen.SetActive(true);
-    //        loadingBar.value = operation.progress;
-    //        yield return null;
-    //    }
-    //}
 
 
     IEnumerator LoadSceneAsyncronously(string sceneName)
@@ -33,11 +23,11 @@ public class SceneLoader : MonoBehaviour
         loadingScreen.SetActive(true);
         loadingBar.value = 0;
 
-        // Fake loading bar that fills over 3 seconds
+  
         float timer = 0f;
         while (timer < 3f)
         {
-            timer += Time.unscaledDeltaTime; // Use unscaledDeltaTime instead
+            timer += Time.unscaledDeltaTime; 
             loadingBar.value = timer / 3f;
             yield return null;
         }
